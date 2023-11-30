c94dd6fe07db574ae86dcae0a089f0528759739d
DabAZ <dabaz@dabaz.me>
2023-11-30 08:40:21 +0800

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer>
       <div className="max-w-5xl px-8 py-12 mx-auto 2xl:max-w-7xl md:px-lg:px-24 lg:pt-48">
-        <p className="max-w-2xl text-sm text-left text-zinc-500">
-          © 
-          <a href="https://dabaz.me" target="_blank">DabAZ</a>.
+        <p className="max-w-2xl text-sm text-left text-neutral-600 dark:text-neutral-400">
+          ©
+          <a href="https://dabaz.me" target="_blank"> DabAZ</a>.
         </p>
       </div>
     </footer>
