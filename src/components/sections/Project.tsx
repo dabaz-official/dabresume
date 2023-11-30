2833668752340db0d31500f0cc27e49a8841d002
DabAZ <dabaz@dabaz.me>
2023-11-30 09:54:26 +0800

    Add project section

---
+++ src/components/sections/Project.tsx
@@ -0,0 +1,24 @@
+import { projects } from "@dr/schemas/contents";
+
+export default function Project() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold tracking-tight text-neutral-900">
+        Projects
+      </h2>
+      <div className="flex flex-col gap-8">
+        {projects.map((project) => (
+          <div key={project.name}>
+            <p className="mt-4 text-lg font-medium bg-gradient-to-r from-indigo-600 dark:from-indigo-400 from-10% via-sky-600 dark:via-sky-400 via-30% to-emerald-600 dark:to-emerald-400 to-90% bg-clip-text text-transparent">
+              {project.name}
+              <span className="text-neutral-600"> - {project.occupation}</span><span className="block text-xs text-neutral-600">{project.period}</span>
+            </p>
+            <p className="mt-4 text-md text-neutral-600">
+              {project.description}
+            </p>
+          </div>    
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/schemas/contents.ts
@@ -0,0 +1,15 @@
+export const projects = [
+  {
+    name: 'DabHack',
+    period: 'July 2023 - Present',
+    occupation: 'Independent Project',
+    description: 'I independently developed "DabHack", a distributed web vulnerability scanner based on Python, which encompasses a multi-stage workflow: subdomain enumeration and brute-forcing > domain resolution > IP and IP range port scanning > service identification, followed by HTTP probing > targeted vulnerability assessment.',
+  },
+  {
+    name: 'DabChat',
+    period: 'November 2022 - November 2023',
+    occupation: 'Independent Project',
+    description: 'DabChat is a web-based chat platform built with Next.js and TypeScript. For the front-end, I made full use of Next.js and strictly utilized TypeScript to ensure type safety and enhance code quality. On the back-end, I chose MySQL as our relational database and seamlessly integrated it using Prisma, a modern database toolkit. A key feature of DabChat is its implementation of end-to-end encryption, ensuring the highest level of privacy and security for user communications.',
+  },
+
+]
